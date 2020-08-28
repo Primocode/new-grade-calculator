9780c965e4d904800b6d6b6d0ec486ccb2c9c468
Primocode <@gmail.com>
2020-08-28 23:26:38 +0200

    DOM structure created

---
+++ main.js
@@ -0,0 +1,21 @@
+// const values = {
+//     which: [],
+//     schoolSubject: [],
+//     rating: [],
+// }
+
+// values.which.push(1)
+// values.schoolSubject.push("matematyka")
+// values.rating.push("5")
+// values.which.push(2)
+// values.schoolSubject.push("Fizyka")
+// values.rating.push("3")
+
+const addingAnItem = () => {
+    const schoolSubjectValue = document.querySelector('.adding-school-subjects-input').value
+
+    console.log(schoolSubjectValue)
+
+}
+
+document.querySelector('.adding-school-subjects-btn').addEventListener('click', addingAnItem)
